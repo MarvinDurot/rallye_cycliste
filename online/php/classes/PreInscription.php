e4c8519c9f3ec549794bea0fe50bc9d2d067fc03
durotm <durotm@localhost.localdomain>
2015-04-02 15:51:57 +0200

    Changements dans les méthodes

--- online/php/classes/PreInscription.php
+++ online/php/classes/PreInscription.php
@@ -5,43 +5,58 @@ class PreInscription extends TableObject {
 	public $hasAutoIncrementedKey = true;
 	
 	/*
-	 * Affiche une pré-inscription sur une ligne de tableau
-	 * avec formulaire caché contenant l'id de la pré-inscription
-	 * et un bouton de suppression
+	 * Affiche une pré-inscription dans une ligne de tableau	
 	 */
-	public function toTableRowForm() {
-		echo '<form action="', $_SERVER['PHP_SELF'], '" method="POST">
-    		  	<input type="hidden" value="', $this->idPreInscription, '" name="id">
-    		  			<tr>
-    		  				<td>', $this->nom, '</td>
-    		  				<td>', $this->prenom, '</td>
-							<td>', $this->sexe, '</td>
-							<td>', $this->dateNaissance, '</td>
-							<td>', $this->federation, '</td>
-							<td>', $this->clubOuVille, '</td>
-							<td>', $this->departement, '</td>
-							<td>', $this->parcours, '</td>
-							<td> <button class="icon-button">
-								 	<span class="glyphicon glyphicon-remove"></span>
-							 	</button>
-							</td>
-						</tr>';
+	public function toTableRow() {
+		$parcours = new ParcoursDAO(MaBD::getInstance());
+		echo '<tr>
+				<td>', $this->nom, '</td>
+    		  	<td>', $this->prenom, '</td>
+			  	<td>', $this->sexe, '</td>
+			  	<td>', $this->dateNaissance, '</td>
+			  	<td>', $this->federation, '</td>
+			  	<td>', $this->clubOuVille, '</td>
+	          	<td>', $this->departement, '</td>
+			  	<td>', $parcours->getOne($this->parcours), '</td>
+			  </tr>';
 	}
 	
 	/*
-	 * Affiche une pré-inscription sur une ligne de tableau
+	 * Affiche une pré-inscription dans un formulaire en ligne
 	 */
-	public function toTableRow() {
-		echo '<tr>
-    		  	<td>', $this->nom, '</td>
-    		  	<td>', $this->prenom, '</td>
-				<td>', $this->sexe, '</td>
-				<td>', $this->dateNaissance, '</td>
-				<td>', $this->federation, '</td>
-				<td>', $this->clubOuVille, '</td>
-				<td>', $this->departement, '</td>
-				<td>', $this->parcours, '</td>
-			  </tr>';
+	public function toForm() {
+		$parcours = new ParcoursDAO(MaBD::getInstance());
+		echo '<div class="row center-text">
+				<div class="col-lg-12">
+					<form method="POST" action="', $_SERVER['PHP_SELF'], '">
+						<input type="hidden" name="id" value="', $this->idPreInscription, '">
+							<div class="col-lg-2">
+								<p>', $this->nom, ' ', $this->prenom, '</p>
+							</div>
+							<div class="col-lg-2">
+								<p>', $this->dateNaissance, '</p>
+							</div>
+							<div class="col-lg-1">
+								<p>', $this->sexe, '</p>
+							</div>
+							<div class="col-lg-1">
+								<p>', $this->federation, '</p>
+							</div>
+							<div class="col-lg-2">
+								<p>', $this->clubOuVille, '</p>
+							</div>
+							<div class="col-lg-1">
+								<p>', $this->departement, '</p>
+							</div>
+							<div class="col-lg-2">
+								<p>', $parcours->getOne($this->parcours), '</p>
+							</div>				
+							<button type="submit" class="icon-button" name="delete[]">
+								<span class="glyphicon glyphicon-remove"></span>
+							</button>						
+			 		</form>
+				</div>
+			</div>';
 	}
 }
 ?>
