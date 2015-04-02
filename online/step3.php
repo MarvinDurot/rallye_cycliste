60d6cbc9a71d4c367f01becbc45bb54b9296aa37
durotm <durotm@localhost.localdomain>
2015-04-02 15:52:27 +0200

    MAJ étape 3 (presque terminée)

--- online/step3.php
+++ online/step3.php
@@ -16,15 +16,32 @@ require_once ('php/functions.php');
 // Initialisation des erreurs
 $_POST ['erreur'] = false;
 
-if (isset ( $_POST ['confirmer'] )) {
-	if (valide ( $email, $_POST ['codeEnvoye'] )) {
-		valider ( $email );
-		$_SESSION ['code'] = $_POST ['codeEnvoye'];
-		header ( "Location: step3.php" );
-		exit ( 0 );
+// Supprimer une inscription
+if (isset ( $_POST ['delete'] )) {
+	supprimerInscription ( $_POST ['id'] );
+	$_POST ['message'] = "L'inscription a bien été supprimée !";
+}
+
+// Ajouter une inscription
+if (isset ( $_POST ['ajouter'] )) {
+	if (checkInputs()) {
+		echo "test";
+		ajouterInscription();
 	} else {
-		$_POST ['erreur'] = true;
-		$_POST ['message'] = "Le code de validation n'est pas correct !";
+		$_POST['erreur'] = true;
+		$_POST['message'] = "La saisie est invalide !";
+	}
+}
+
+// Valider les inscriptions
+if (isset ( $_POST ['valider'] )) {
+	if (checkRef($_SESSION['email'])) {
+		$_SESSION['recap'] = true;
+		header ( "Location: step4.php" );
+		exit(0);
+	} else {
+		$_POST['erreur'] = true;
+		$_POST['message'] = "Vous n'avez saisi aucune inscription !";
 	}
 }
 ?>
@@ -45,78 +62,66 @@ if (isset ( $_POST ['confirmer'] )) {
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-12">				
-				<?php afficherPreInscriptions('user2@mail.com'); ?>				
+			<div class="col-lg-4 col-lg-offset-1">
+				<form method="POST" action="step3.php">						
+					<div class="form-group col-sm-5">
+						<label>Nom :</label> <input class="form-control input-sm"
+							name="nom" type="text" placeholder="Nom..." autofocus>
+					</div>
+					<div class="form-group col-sm-5">
+						<label>Prénom :</label> <input class="form-control input-sm"
+							name="prenom" type="text" placeholder="Prénom...">
+					</div>
+					<div class="form-group">
+						<label>Sexe :</label> <select class="sel" name="sexe">
+							<option value="H">H</option>
+							<option value="F">F</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Date de naissance :</label> <input
+							class="form-control input-sm" name="dateNaissance" type="date"
+							placeholder="AAAA-MM-JJ">
+					</div>
+					<div class="form-group">
+						<label>Fédé. :</label> <select class="sel" name="federation">
+									<?php chargerFederations();?>
+								</select>
+					</div>
+					<div class="form-group">
+						<label>Ville ou Club</label> <input class="form-control input-sm"
+							name="clubOuVille" type="text" placeholder="Ville ou Club...">
+					</div>
+					<div class="form-group">
+						<label>Dép.</label> <input class="form-control input-sm"
+							name="departement" type="text" placeholder="Ex: 07">
+					</div>
+					<div class="form-group">
+						<label>Parcours</label> <select class="sel" name="parcours">
+									<?php chargerParcours();?>
+								</select>
+					</div>
+					<input type="hidden" value="<?php echo $_SESSION['email']; ?>"
+						name="inscriveur" id="inscriveur">
+					<input type="hidden" name="estArrive" value="0">
+
+
+					<div class="pull-right">
+						<button type="submit" class="btn btn-primary" name="ajouter[]">Valider</button>
+					</div>
+				</form>
+			</div>
+			<div class="col-lg-7">				
+				<?php afficherPreInscriptions($_SESSION['email']); ?>				
 			</div>
 		</div>
-		<div class="col-lg-12">
-			<form>
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h3 class="panel-title">
-							<span class="glyphicon glyphicon-ok">&nbsp;</span> Inscrire un
-							cycliste
-						</h3>
-					</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<div class="col-lg-2">
-								<label>Nom</label> <input class="form-control input-sm" id="nom"
-									name="nom" type="text" placeholder="Nom..." autofocus>
-							</div>
-							<div class="col-lg-2">
-								<label>Prénom</label> <input class="form-control input-sm"
-									id="prenom" name="prenom" type="text" placeholder="Prénom...">
-							</div>
-							<div class="col-lg-1">
-								<label>Sexe</label> <select class="sel" id="sexe">
-									<option value="H">H</option>
-									<option value="F">F</option>
-								</select>
-							</div>
-							<div class="col-lg-2">
-								<label>Date de naissance</label> <input
-									class="form-control input-sm" id="date" name="date" type="date"
-									placeholder="AAAA-MM-JJ">
-							</div>
 
-							<div class="col-lg-1">
-								<label>Fédé.</label><select class="sel" id="federation"></select>
-							</div>
-							<div class="col-lg-2">
-								<label>Ville ou Club</label> <input
-									class="form-control input-sm" id="clubOuVille"
-									name="clubOuVille" type="text" placeholder="Ville ou Club...">
-							</div>
-							<div class="col-lg-1">
-								<label>Dép.</label> <input class="form-control input-sm"
-									id="departement" name="departement" type="text"
-									placeholder="Ex: 07">
-							</div>
-							<div class="col-lg-1">
-								<label>Parcours</label> <select class="sel" id="parcours"></select>
-							</div>
-
-							<input type="text" value="<?php echo $_SESSION['email']; ?>"
-								name="inscriveur" id="inscriveur" class="hide">
-
-						</div>
-					</div>
-
-					<div class="panel-footer clearfix">
-						<div class="pull-right">
-							<button type="submit" class="btn btn-primary" id="valider">Valider</button>
-						</div>
-					</div>
-				</div>
-			</form>
-
-			<div id="message" class="row vertical-offset-50">
-				<div class="col-lg-offset-1 col-lg-3">
+		<div id="message" class="row vertical-offset-50">
+			<div class="col-lg-12">
 				<?php afficherMessage(); ?>
 			</div>
-			</div>
 		</div>
+	</div>
 
 </body>
 </html>
