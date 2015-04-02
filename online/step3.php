a56d5886a61daf835c95228f89e4e2df0e35678a
durotm <durotm@localhost.localdomain>
2015-04-02 17:16:10 +0200

    Fin étape 3

--- online/step3.php
+++ online/step3.php
@@ -24,24 +24,24 @@ if (isset ( $_POST ['delete'] )) {
 
 // Ajouter une inscription
 if (isset ( $_POST ['ajouter'] )) {
-	if (checkInputs()) {
-		echo "test";
-		ajouterInscription();
+	if (checkInputs ()) {
+		ajouterInscription ();
+		$_POST ['message'] = "L'inscription a bien été ajoutée !";
 	} else {
-		$_POST['erreur'] = true;
-		$_POST['message'] = "La saisie est invalide !";
+		$_POST ['erreur'] = true;
+		$_POST ['message'] = "La saisie est invalide !";
 	}
 }
 
 // Valider les inscriptions
 if (isset ( $_POST ['valider'] )) {
-	if (checkRef($_SESSION['email'])) {
-		$_SESSION['recap'] = true;
+	if (checkRef ( $_SESSION ['email'] )) {
+		$_SESSION ['recap'] = true;
 		header ( "Location: step4.php" );
-		exit(0);
+		exit ( 0 );
 	} else {
-		$_POST['erreur'] = true;
-		$_POST['message'] = "Vous n'avez saisi aucune inscription !";
+		$_POST ['erreur'] = true;
+		$_POST ['message'] = "Vous n'avez saisi aucune inscription !";
 	}
 }
 ?>
@@ -61,16 +61,16 @@ if (isset ( $_POST ['valider'] )) {
 				</div>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row vertical-offset-20">
 			<div class="col-lg-4 col-lg-offset-1">
-				<form method="POST" action="step3.php">						
-					<div class="form-group col-sm-5">
+				<form method="POST" action="step3.php">
+					<div class="form-group">
 						<label>Nom :</label> <input class="form-control input-sm"
-							name="nom" type="text" placeholder="Nom..." autofocus>
+							name="nom" type="text" placeholder="Nom..." required autofocus>
 					</div>
-					<div class="form-group col-sm-5">
+					<div class="form-group">
 						<label>Prénom :</label> <input class="form-control input-sm"
-							name="prenom" type="text" placeholder="Prénom...">
+							name="prenom" type="text" placeholder="Prénom..." required>
 					</div>
 					<div class="form-group">
 						<label>Sexe :</label> <select class="sel" name="sexe">
@@ -81,7 +81,7 @@ if (isset ( $_POST ['valider'] )) {
 					<div class="form-group">
 						<label>Date de naissance :</label> <input
 							class="form-control input-sm" name="dateNaissance" type="date"
-							placeholder="AAAA-MM-JJ">
+							placeholder="AAAA-MM-JJ" required>
 					</div>
 					<div class="form-group">
 						<label>Fédé. :</label> <select class="sel" name="federation">
@@ -90,11 +90,12 @@ if (isset ( $_POST ['valider'] )) {
 					</div>
 					<div class="form-group">
 						<label>Ville ou Club</label> <input class="form-control input-sm"
-							name="clubOuVille" type="text" placeholder="Ville ou Club...">
+							name="clubOuVille" type="text" placeholder="Ville ou Club..."
+							required>
 					</div>
 					<div class="form-group">
 						<label>Dép.</label> <input class="form-control input-sm"
-							name="departement" type="text" placeholder="Ex: 07">
+							name="departement" type="text" placeholder="Ex: 07" required>
 					</div>
 					<div class="form-group">
 						<label>Parcours</label> <select class="sel" name="parcours">
@@ -102,17 +103,60 @@ if (isset ( $_POST ['valider'] )) {
 								</select>
 					</div>
 					<input type="hidden" value="<?php echo $_SESSION['email']; ?>"
-						name="inscriveur" id="inscriveur">
-					<input type="hidden" name="estArrive" value="0">
-
-
+						name="inscriveur" id="inscriveur"> <input type="hidden"
+						name="estArrive" value="0">
 					<div class="pull-right">
-						<button type="submit" class="btn btn-primary" name="ajouter[]">Valider</button>
+						<button type="submit" class="btn btn-success" name="ajouter[]">Ajouter</button>
 					</div>
 				</form>
 			</div>
-			<div class="col-lg-7">				
-				<?php afficherPreInscriptions($_SESSION['email']); ?>				
+			<div class="col-lg-7">
+				<div class="row center-text">
+					<div class="col-lg-12">
+						<div class="col-lg-2">
+							<p>
+								<strong>Nom</strong>
+							</p>
+						</div>
+						<div class="col-lg-2">
+							<p>
+								<strong>Date Naiss.</strong>
+							</p>
+						</div>
+						<div class="col-lg-1">
+							<p>
+								<strong>Sexe</strong>
+							</p>
+						</div>
+						<div class="col-lg-1">
+							<p>
+								<strong>Fédé.</strong>
+							</p>
+						</div>
+						<div class="col-lg-2">
+							<p>
+								<strong>Club/Ville</strong>
+							</p>
+						</div>
+						<div class="col-lg-1">
+							<p>
+								<strong>Dép.</strong>
+							</p>
+						</div>
+						<div class="col-lg-2">
+							<p>
+								<strong>Parcours</strong>
+							</p>
+						</div>
+					</div>
+				</div>
+				<?php afficherPreInscriptions($_SESSION['email']); ?>
+				<form method="POST" action="step3.php">
+					<div class="pull-right">
+						<button type="submit" class="btn btn-primary" name="valider[]">Valider
+							tout</button>
+					</div>
+				</form>
 			</div>
 		</div>
 
@@ -122,6 +166,5 @@ if (isset ( $_POST ['valider'] )) {
 			</div>
 		</div>
 	</div>
-
 </body>
 </html>
